84b520802b81a2d55d81bb7a0b1bb21778ca9b61
Petra Petrusova <petrusovap03@gmail.com>
2023-12-05 16:08:13 +0000

    Add code to ColourTable to check validity of parameter noColours

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -2,6 +2,10 @@ public class ColourTable {
     private int[] palette;
 
     public ColourTable(int noColours) {
+        if (!isPowerOfTwo(noColours) || noColours <= 1 || noColours >= 1025) {
+            throw new IllegalArgumentException("Number of colours in the palette must be" +
+                    " a power of two and greater than 1 and less than 1025.");
+        }
         palette = new int[noColours];
     }
 
@@ -9,4 +13,20 @@ public class ColourTable {
         return palette.length;
     }
 
+    private boolean isPowerOfTwo(int number) {
+
+        if (number <= 0) {
+            return false;
+        }
+
+        if (number % 2 != 0) {
+            return false;
+        }
+
+        while (number % 2 == 0)  {
+            number = number/2;
+        }
+
+        return number == 1;
+    }
 }
