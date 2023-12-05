ba20398c9352bcf5b24e2e1526f4c681e1408bab
Petra Petrusova <petrusovap03@gmail.com>
2023-12-05 15:43:13 +0000

    Create getPaletteSize method in ColourTable and add palette variable

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -1,7 +1,12 @@
 public class ColourTable {
+    private int[] palette;
 
     public ColourTable(int noColours) {
+        palette = new int[noColours];
+    }
 
+    public int getPaletteSize() {
+        return palette.length;
     }
 
 }
