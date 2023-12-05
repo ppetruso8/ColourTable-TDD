a3818aa18e7ed3efdf5b662e66fa6c6f3da393c4
Petra Petrusova <petrusovap03@gmail.com>
2023-12-05 19:34:30 +0000

    Add messages to the assert statements

--- src/test/java/TestColourTable.java
+++ src/test/java/TestColourTable.java
@@ -19,14 +19,16 @@ public class TestColourTable {
     @DisplayName("Ensure the palette size is initialized correctly")
     public void testArray() {
         int expectedSize = 4;
-        assertEquals(expectedSize, colourTable.getPaletteSize());
+        assertEquals(expectedSize, colourTable.getPaletteSize(),
+                "Palette array is not initialized with correct size");
     }
 
     @ParameterizedTest
     @DisplayName("Ensure that exception is thrown when noColours has an invalid value")
     @ValueSource(ints = {1, 7, 1025})
     public void invalidNoColoursValue(int noColours) {
-        assertThrows(IllegalArgumentException.class, () -> new ColourTable(noColours));
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(noColours),
+                "Invalid value passed in while creating ColourTable is no handled correctly");
     }
 
     @Test
@@ -34,7 +36,8 @@ public class TestColourTable {
     public void testAddColour() throws Exception {
         int colourToAdd = 0xFFFFFF;
         colourTable.add(colourToAdd);
-        assertEquals(colourToAdd, colourTable.getPalette(0));
+        assertEquals(colourToAdd, colourTable.getPalette(0),
+                "Colour is not added to the palette");
     }
 
     @Test
@@ -46,14 +49,16 @@ public class TestColourTable {
         colourTable.add(0x000002);
         colourTable.add(0x000003);
         // add colour to full palette
-        assertThrows(Exception.class, () -> colourTable.add(0x000004));
+        assertThrows(Exception.class, () -> colourTable.add(0x000004),
+                "Colour should not be added into a full palette");
     }
 
     @Test
     @DisplayName("Ensure the exception is thrown when invalid RGB value is attempted to be added to palette")
     public void addInvalidRGBValue() {
         int invalidRGB = 0xFFFFFFF;
-        assertThrows(IllegalArgumentException.class, () -> colourTable.add(invalidRGB));
+        assertThrows(IllegalArgumentException.class, () -> colourTable.add(invalidRGB),
+                "Invalid RGB value is not checked correctly");
     }
 
     @Test
@@ -64,7 +69,8 @@ public class TestColourTable {
         colourTable.add(duplicateColour);
         // add the same value to the palette again
         colourTable.add(duplicateColour);
-        assertNull(colourTable.getPalette(1));
+        assertNull(colourTable.getPalette(1),
+                "Duplicated colour is not being ignored");
     }
 
     @Test
@@ -72,6 +78,7 @@ public class TestColourTable {
     public void addZero() throws Exception {
         colourTable.add(0x000000);
         colourTable.add(0x000001);
-        assertEquals(1, colourTable.getPalette(1));
+        assertEquals(1, colourTable.getPalette(1),
+                "Addition of RGB value of 0 to the palette is not correctly handled");
     }
 }
